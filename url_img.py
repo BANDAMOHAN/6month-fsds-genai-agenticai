ee5e300ad1f4b98098c5be18027938f66cb39e72
BANDAMOHAN <bandamohan57@gmail.com>
2025-07-23 23:16:56 +0530

    Add files via upload

---
+++ url_img.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import requests
+from io import BytesIO
+
+
+def load_image_from_url(url):
+    response=requests.get(url)
+    return Image.open(BytesIO(response.content))
+
+elephant_url="https://cdn.britannica.com/02/152302-050-1A984FCB/African-savanna-elephant.jpg"
+elephant=load_image_from_url(elephant_url)
+
+plt.figure(figsize=(6,4))
+plt.imshow(elephant)
+plt.title('elephant')
+plt.axis('off')
+plt.show()
